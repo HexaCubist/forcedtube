21b1e485fa5305bcae562dd9d27bbb99b32e8782
Zac Pullar-Strecker <zacmps@gmail.com>
2023-12-09 15:28:39 +1300

    changes

--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -11,17 +11,21 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log("adding webRequest listener");
 
   chrome.webRequest.onCompleted.addListener(async (details) => {
-    console.log("got timedtext request");
+    if (details.tabId == sender.tab.id) {
+      console.log("got timedtext request for correct tab");
 
-    const response = await fetch(details.url);
-    const r = await response.json();
-    const transcript = r.events.map(e => (e.segs ?? []).map(s => s.utf8));
+      const response = await fetch(details.url);
+      const r = await response.json();
+      const transcript = r.events.map(e => (e.segs ?? []).map(s => s.utf8));
 
-    sendResponse({
-      transcript: transcript
-    });
+      sendResponse({
+        transcript: transcript
+      });
+    } else {
+      console.log("got timedtext request for incorrect tab");
+    }
   }, {urls: ['https://www.youtube.com/api/timedtext']})
 
-  // keep the script alive?
+  // keep the script alive for an async response
   return true;
 })

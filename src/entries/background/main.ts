72b43001746b44b6347686a36176bf1af35b1793
Zac Pullar-Strecker <zacmps@gmail.com>
2023-12-09 20:46:02 +1300

    webRequest

--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -1,9 +1,3 @@
-import browser from "webextension-polyfill";
-
-browser.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed");
-});
-
 chrome.webRequest.onCompleted.addListener(async (details) => {
   console.log("got timedtext request");
 
@@ -13,27 +7,10 @@ chrome.webRequest.onCompleted.addListener(async (details) => {
 
   const previous = JSON.parse(localStorage.getItem(details.tabId.toString()) ?? "[]");
 
-  previous.append(transcript)
+  previous.append(transcript);
 
-  chrome.storage.local.set({[details.tabId.toString()]: JSON.stringify(previous)});
+  chrome.tabs.sendMessage(details.tabId, previous);
+  console.log("send timedtext request");
 }, {urls: ['https://www.youtube.com/api/timedtext']})
-
-chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-  console.log("got message", msg, "from", sender);
-  (async () => {
-    try {
-      console.log("trying to fetch transcripts");
-      const r = await chrome.storage.local.get(sender.tab.id.toString());
-      console.log("fetched transcripts");
-      sendResponse({transcripts: []});
-    } catch (err) {
-      console.log("no stored transcripts");
-      sendResponse({transcripts: []});
-    }
-  })();
-
-  // persist the background script;
-  return true;
-});
 
 console.log("ran background script");
--- src/entries/contentScript/primary/networkListener.ts
+++ src/entries/contentScript/primary/networkListener.ts
@@ -1,16 +1,8 @@
 const setupNetworkListener = (callback) => {
     console.log("setupNetworkListener");
-    // send a dummy message to identify the tab to the background script
-    chrome.runtime.sendMessage({}, (response) => {
-        if (response && response.transcripts) {
-            console.log("got background script transcript");
-            if (response.transcripts.length > 0) {
-                // todo, identify the ad transcript
-                callback(response.transcripts[0]);
-            }
-        } else {
-            console.error("failed to get transcript from background script", response);
-        }
+
+    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+        console.log("got message", msg);
     });
 }
 
